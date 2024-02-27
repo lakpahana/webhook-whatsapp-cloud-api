c70c40fb6913d1898049620b57c94b40a7025804
AGS Lakpahana <agslakpahana@gmail.com>
2024-02-28 01:47:33 +0530

    webhook basic

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const token = ""
+
+app.get('/webhooks',  (req, res) => {
+ if (
+   req.query['hub.mode'] == 'subscribe' &&
+   req.query['hub.verify_token'] == token
+ ) {
+   res.send(req.query['hub.challenge']);
+ } else {
+   res.sendStatus(400);
+ }
+})
+
+app.post('/webhooks', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 
